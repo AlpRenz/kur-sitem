be3b3e14f6dac6cadfa1e649990662172ba715fe
AlpRenz <133846358+AlpRenz@users.noreply.github.com>
2026-08-10 15:59:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Supabase Dashboard > Project Settings > API bölümündeki değerleri buraya ekleyin.
 window.KUR_CONFIG = {
-  supabaseUrl: 'SUPABASE_PROJECT_URL',
-  supabaseAnonKey: 'SUPABASE_ANON_KEY'
+  supabaseUrl: 'sb_publishable_fbZFNtV5xJlySaYDvWzTWg_sxycTkiI',
+  supabaseAnonKey: 'https://torjjifkvsbbdkkddaok.supabase.co'
 };
